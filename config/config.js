3d59276a56242dcda490765033143f5c2d4f039b
Emily Wu <emilywu0227@gmail.com>
2022-01-13 21:22:09 -0500

    finished config

---
+++ config/config.js
@@ -0,0 +1,17 @@
+// PROMISE LIBRARY
+const promise = require("bluebird");
+
+// OVERRIDING DEFAULT PROMISE LIBRARY
+const options = {
+  promiseLib: promise,
+  query: (e) => {
+    console.log(e.query);
+  },
+};
+const pgp = require("pg-promise")(options);
+
+// SET UP THE CONNECTION STRING FOR THE DATABASE
+const connectionString = process.env.DATABASE_URL;
+const db = pgp(connectionString);
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config(); // SUPPORT .ENV FILES
+const express = require("express"); // BRING IN EXPRESS
+const app = express(); // INITILIZE APP
+const path = require("path");
+const bodyParser = require("body-parser");
+const players = require("./routes/api/players");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("api/player", players);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
