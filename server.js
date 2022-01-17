5369690bc25d3d832b47acbce51ee9dea80dc974
Emily Wu <emilywu0227@gmail.com>
2022-01-17 16:03:14 -0500

    change server config

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express"); // BRING IN EXPRESS
 const app = express(); // INITILIZE APP
 const path = require("path");
 const bodyParser = require("body-parser");
-const pg = require("pg");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
 const players = require("./routes/api/players");
@@ -19,18 +18,16 @@ if (process.env.NODE_ENV === "production") {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(
-  session({
-    store: new pgSession({
-      conString: process.env.DATABASE_URL,
-    }),
-    key: "user_sid",
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 10 * 10 * 6000000 },
-  })
-);
+// app.use(
+//   session({
+//     store: new pgSession({
+//       conString: process.env.DATABASE_URL,
+//     }),
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     cookie: { maxAge: 10 * 10 * 6000000 },
+//   })
+// );
 
 // app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/players", players);
